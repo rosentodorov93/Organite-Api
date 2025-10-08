1850d8ab4946b37cabe65151906e61a88d5aff5c
rosentodorov93 <rosen.vt55@gmail.com>
2025-10-08 17:02:05 +0300

    Add notes model and init database

---
+++ Organite.Api/Organite.Domain/Entities/Note.cs
@@ -0,0 +1,12 @@
+﻿namespace Organite.Domain.Entities;
+
+public class Note
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = default!;
+    public string Content { get; set; } = default!;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public List<string> Tags { get; set; } = new();
+    public bool IsPinned { get; set; }
+}
---
+++ Organite.Api/Organite.Infrastructure/Data/OrganiteDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Organite.Domain.Entities;
+
+namespace Organite.Infrastructure.Data;
+
+internal class OrganiteDBContext : DbContext
+{
+    internal DbSet<Note> Notes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost;Database=OrganiteDB;Integrated Security=SSPI;MultipleActiveResultSets=true;TrustServerCertificate=True");
+    }
+}
