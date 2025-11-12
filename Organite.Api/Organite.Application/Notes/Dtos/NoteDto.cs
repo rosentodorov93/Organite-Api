7733843528f8776dc3990decac215ae7c73fc1ff
rosentodorov93 <rosen.vt55@gmail.com>
2025-11-12 13:33:16 +0200

    Clean unused method for mapping

--- Organite.Api/Organite.Application/Notes/Dtos/NoteDto.cs
+++ Organite.Api/Organite.Application/Notes/Dtos/NoteDto.cs
@@ -11,19 +11,4 @@ public class NoteDto
     public DateTime UpdatedAt { get; set; }
     public List<string> Tags { get; set; } = new();
     public bool IsPinned { get; set; }
-
-    public static NoteDto? FromEntity(Note? note)
-    {
-        if (note == null) return null;
-        return new NoteDto()
-        {
-            Id = note.Id,
-            Title = note.Title,
-            Content = note.Content,
-            CreatedAt = note.CreatedAt,
-            UpdatedAt = note.UpdatedAt,
-            Tags = note.Tags,
-            IsPinned = note.IsPinned,
-        };
-    }
 }
